729c3fadc6f75277254c3003ae6b0a2cdb5aae75
o-ray-o <rayfong_goh@mymail.sutd.edu.sg>
2023-12-27 16:05:44 +0800

    First commit

---
+++ backend/scripts/deploy.js
@@ -0,0 +1,40 @@
+const hre = require("hardhat");
+const fs = require("fs/promises");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const Voting = await hre.ethers.getContractFactory("Voting");
+  const voting = await Voting.deploy();
+
+  await voting.deployed();
+  await writeDeploymentInfo(voting, "voting.json");
+}
+
+async function writeDeploymentInfo(contract, filename = "") {
+  const data = {
+    contract: {
+      address: contract.address,
+      signerAddress: contract.signer.address,
+      abi: contract.interface.format(),
+    },
+  };
+
+  const content = JSON.stringify(data, null, 2);
+  await fs.writeFile(filename, content, { encoding: "utf-8" });
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
